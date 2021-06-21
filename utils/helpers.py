c02cbd6aebe1bca720ffaa7ff0fb5f6e8fadeee5
Jonny Li <myself@jonny.li>
2021-06-21 03:09:39 -0700

    Add pgn dataset preprocessing scripts

---
+++ utils/helpers.py
@@ -0,0 +1,135 @@
+import chess
+import numpy as np
+
+
+def get_board_state(board: chess.Board):
+    """
+    get board state representation from the pov of current player (board.turn).
+
+    state vector description:
+    0-63 - board pieces from my pov (a1-h8 for white, reversed for black)
+    64-71 - squares my pawns can en passant to
+    72-73 - my castling rights (left, right)
+    74-75 - opponent's castling rights (left, right)
+
+    vocabulary description:
+    0 - empty space
+    1-6 - my pieces
+    7-12 - opponent's pieces
+    13 - en passant unavailable
+    14 - en passant available
+    15 - castle unavailable
+    16 - castle available
+    """
+    pieces = [0] * 64
+    for square, piece in board.piece_map().items():
+        pieces[square] = piece.piece_type + (piece.color ^ board.turn) * 6
+    ep = [13] * 8
+    if board.ep_square:
+        ep[board.ep_square % 8] = 14
+    castle = [16 if board.castling_rights & c else 15
+              for c in [chess.BB_A1, chess.BB_H1, chess.BB_A8, chess.BB_H8]]
+    if board.turn == chess.BLACK:
+        pieces = pieces[::-1]
+        ep = ep[::-1]
+        castle = castle[::-1]
+    state = pieces + ep + castle
+    return state
+
+
+MOVE_DIRECTION = [
+    UP, UP_LEFT, UP_RIGHT, LEFT, RIGHT, DOWN, DOWN_LEFT, DOWN_RIGHT, UP2_LEFT,
+    UP2_RIGHT, UP_LEFT2, UP_RIGHT2, DOWN2_LEFT, DOWN2_RIGHT, DOWN_LEFT2,
+    DOWN_RIGHT2, UP_LEFT_PROMOTE_QUEEN, UP_LEFT_PROMOTE_ROOK,
+    UP_LEFT_PROMOTE_KNIGHT, UP_LEFT_PROMOTE_BISHOP, UP_PROMOTE_QUEEN,
+    UP_PROMOTE_ROOK, UP_PROMOTE_KNIGHT, UP_PROMOTE_BISHOP,
+    UP_RIGHT_PROMOTE_QUEEN, UP_RIGHT_PROMOTE_ROOK, UP_RIGHT_PROMOTE_KNIGHT,
+    UP_RIGHT_PROMOTE_BISHOP
+] = range(28)
+
+
+def get_move(move: chess.Move, color: chess.Color):
+    """
+    move vocab description:
+
+    see MOVE_DIRECTION constants
+    """
+    from_sq = move.from_square
+    to_sq = move.to_square
+    if color == chess.BLACK:
+        to_sq = 64 - to_sq
+        from_sq = 64 - from_sq
+    from_rank, from_file = divmod(from_sq, 8)
+    to_rank, to_file = divmod(to_sq, 8)
+    dir_file = to_file - from_file
+    dir_rank = to_rank - from_rank
+    if move.promotion is not None:
+        if move.promotion == chess.QUEEN:
+            move_direction = UP_PROMOTE_QUEEN
+        elif move.promotion == chess.ROOK:
+            move_direction = UP_PROMOTE_ROOK
+        elif move.promotion == chess.KNIGHT:
+            move_direction = UP_PROMOTE_KNIGHT
+        elif move.promotion == chess.BISHOP:
+            move_direction = UP_PROMOTE_BISHOP
+        else:
+            raise RuntimeError
+        move_direction += 4 * dir_file
+    elif dir_rank == 2 and dir_file == -1:
+        move_direction = UP2_LEFT
+    elif dir_rank == 2 and dir_file == 1:
+        move_direction = UP2_RIGHT
+    elif dir_rank == -2 and dir_file == -1:
+        move_direction = DOWN2_LEFT
+    elif dir_rank == -2 and dir_file == 1:
+        move_direction = DOWN2_RIGHT
+    elif dir_rank == 1 and dir_file == -2:
+        move_direction = UP_LEFT2
+    elif dir_rank == 1 and dir_file == 2:
+        move_direction = UP_RIGHT2
+    elif dir_rank == -1 and dir_file == -2:
+        move_direction = DOWN_LEFT2
+    elif dir_rank == -1 and dir_file == 2:
+        move_direction = DOWN_RIGHT2
+    elif dir_rank > 0 and dir_file == 0:
+        move_direction = UP
+    elif dir_rank > 0 and dir_file < 0:
+        move_direction = UP_LEFT
+    elif dir_rank > 0 and dir_file > 0:
+        move_direction = UP_RIGHT
+    elif dir_rank == 0 and dir_file < 0:
+        move_direction = LEFT
+    elif dir_rank == 0 and dir_file > 0:
+        move_direction = RIGHT
+    elif dir_rank < 0 and dir_file == 0:
+        move_direction = DOWN
+    elif dir_rank < 0 and dir_file < 0:
+        move_direction = DOWN_LEFT
+    elif dir_rank < 0 and dir_file > 0:
+        move_direction = DOWN_RIGHT
+    else:
+        raise RuntimeError
+    return move_direction * 64 + to_sq
+
+
+def get_value(comment: str, color: chess.Color):
+    try:
+        value = float(comment.split('/')[0])
+        if color == chess.BLACK:
+            value *= -1
+        return 1 / (1 + np.exp(-value * 0.5))
+    except ValueError:
+        return 0.5
+
+
+def get_result(result: str, color: chess.Color):
+    if result == '1-0' and color == chess.WHITE:
+        return 1.
+    elif result == '0-1' and color == chess.BLACK:
+        return 1.
+    elif result == '1-0' and color == chess.BLACK:
+        return 0.
+    elif result == '0-1' and color == chess.WHITE:
+        return 0.
+    else:
+        return 0.5
---
+++ utils/preprocess_pgn.py
@@ -0,0 +1,95 @@
+import os
+import argparse
+import glob
+import itertools
+import io
+import random
+
+import chess
+import chess.pgn
+import numpy as np
+from sklearn.model_selection import train_test_split
+import h5py
+
+from .helpers import get_board_state, get_move, get_value, get_result
+
+
+def get_policy_value_labels(pgn_str: str):
+    """
+    get policy/value labels from all positions in a game.
+    """
+    game = chess.pgn.read_game(io.StringIO(pgn_str))
+    rows = []
+    result_header = game.headers.get('Result', None)
+    board = chess.Board()
+    for node in game.mainline():
+        next_node = node.next()
+        if not next_node:
+            continue
+        try:
+            board.push(node.move)
+        except (AssertionError, ValueError) as e:
+            board = node.board()
+        state = get_board_state(board)
+        move = get_move(next_node.move, board.turn)
+        value = get_value(node.comment, board.turn) if node.comment else 0.5
+        result = get_result(result_header, board.turn) if result_header else 0.5
+        rows.append((*state, move, value, result))
+    return rows
+
+
+def main(pgn_dir, output_dir, n_parts=5):
+    print('Loading pgns')
+    pgn_paths = glob.glob(os.path.join(pgn_dir, '*.pgn'))
+    games = []
+    for pgn_path in pgn_paths:
+        with open(pgn_path, 'r') as pgn:
+            pgn_str = ''
+            while True:
+                try:
+                    line = pgn.readline()
+                except UnicodeDecodeError as e:
+                    print(e)
+                    continue
+                if not line:
+                    break
+                if not line.strip() and not pgn_str.strip().endswith(']'):
+                    games.append(pgn_str)
+                    pgn_str = ''
+                else:
+                    pgn_str += line
+    print(f'Loaded {len(games)} games')
+    random.shuffle(games)
+    parts = [games[i::n_parts] for i in range(n_parts)]
+    total_length = 0
+    for i, part in enumerate(parts):
+        print(f'Processing part {i+1}/{len(parts)}')
+        rows = []
+        for j, game in enumerate(part):
+            rows.extend(get_policy_value_labels(game))
+            if j % 10000 == 0:
+                print(f'{j}/{len(part)} games')
+        random.shuffle(rows)
+        rows = np.array(rows,
+            dtype=[*[(f'state_{i}', 'u2') for i in range(len(rows[0]) - 3)],
+                   ('move', 'u2'), ('value', 'f4'), ('result', 'f2')])
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        h5_path = os.path.join(output_dir, f'{i}.h5')
+        with h5py.File(h5_path, 'w') as hf:
+            hf.create_dataset('policy_value_labels', data=rows)
+        print(f'Saved {len(rows)} positions to {h5_path}')
+        total_length += len(rows)
+    print(f'Saved {total_length} positions to {n_parts} parts in {output_dir}')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--pgn_dir',
+                        help='Path to directory of pgns',
+                        required=True)
+    parser.add_argument('-o', '--output_dir',
+                        help='Path to output directory',
+                        required=True)
+    args = parser.parse_args()
+    main(args.pgn_dir, args.output_dir)
