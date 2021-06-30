1babf0d8340f0f8515d6bc144b63672958b2b69c
Jonny Li <myself@jonny.li>
2021-06-30 15:53:17 -0700

    Use virtual losses and multithreading
    Note: usage of threads may not be accurate, might need to fix

--- mcts.py
+++ mcts.py
@@ -1,4 +1,5 @@
 import time
+from threading import Thread, Lock
 
 import chess
 import chess.polyglot
@@ -17,20 +18,23 @@ class Node:
     def __init__(self, board: chess.Board):
         self.moves = list(board.legal_moves)  # [chess.Move]
         self.children = [None for _ in self.moves]  # [Node]
-        self.policy = None  # [float] or None
-        self.value = None  # float or None
-        self.count = 1  # int
+        self.policy = np.ones(len(self.moves)) / len(self.moves)  # [float]
+        self.value = 0  # float
+        self.count = 0  # int
+        self.lock = Lock()
 
 
 class MCTSAgent:
-    def __init__(self, ckpt_path, n_playouts=1000, c_puct=1):
+    def __init__(self, ckpt_path, n_playouts=800, c_puct=1, batch_size=8):
         self.model = BertPolicyValue.load_from_checkpoint(ckpt_path)
         self.model.cuda()
         self.model.eval()
         self.n_playouts = n_playouts
         self.c_puct = c_puct
         self.node_lookup = {}  # zobrist hash -> Node
+        self.node_lookup_lock = Lock()
         self.board = chess.Board()  # use global board reference to save memory
+        self.batch_size = batch_size
 
     def set_position(self, fen_or_moves):
         if isinstance(fen_or_moves, str):
@@ -44,14 +48,25 @@ class MCTSAgent:
         """get best move and info dict given the board position."""
         # initialize
         self.node_lookup.clear()
-        root = self.expand_and_evaluate_node()
+        root = self.expand_node()
+        self.evaluate_node_batch([[root]], [self.board])
         # return early if only 1 legal move
         if len(root.moves) == 1:
             return root.moves[0], {}
         # execute playouts
         start_time = time.time()
-        for _ in range(self.n_playouts):
-            self.search(root)
+        threads = []
+        for i in range(self.n_playouts // self.batch_size):
+            # select leaf nodes and apply virtual losses
+            paths, boards = zip(*(self.select_leaf_node(root)
+                                  for j in range(self.batch_size)))
+            # evaluate batch and backpropagate
+            t = Thread(target=self.evaluate_node_batch, args=(paths, boards))
+            threads.append(t)
+            t.start()
+            #self.evaluate_node_batch(paths, boards)
+        for t in threads:
+            t.join()
         total_time = time.time() - start_time
         # get best move (child node with highest visit count)
         i = np.argmax([c.count if c else 0 for c in root.children])
@@ -72,63 +87,109 @@ class MCTSAgent:
         }
         return bestmove, info
 
-    def search(self, node: Node, invert_child_value=True):
-        """execute one iteration of MCTS"""
+    def select_leaf_node(self, node: Node):
+        """
+        traverse to leaf node and expand tree.
+        returns path from root to leaf node and board state of leaf node.
+        """
+        path = [node]
+        # apply virtual loss
+        node.lock.acquire()
+        node.value -= 1
+        node.count += 1
+        node.lock.release()
+        # terminal state reached
+        if self.board.is_game_over():
+            return path, self.board.copy()
         # select max ucb child
+        node.lock.acquire()
         i = self.select_max_ucb_child(node)
+        child = node.children[i]
         self.board.push(node.moves[i])
-        # if child has not been visited before, expand and evaluate
-        if node.children[i] is None:
-            node.children[i] = self.expand_and_evaluate_node(invert_child_value)
-            child_value = node.children[i].value
+        node.lock.release()
+        # if child has not been visited before, expand and apply virtual loss
+        if child is None:
+            leaf_node = self.expand_node()
+            node.lock.acquire()
+            node.children[i] = leaf_node
+            node.lock.release()
+            path.append(leaf_node)
+            leaf_board = self.board.copy()
         # otherwise recurse on child
         else:
-            child_value = self.search(node.children[i], not invert_child_value)
-        # backpropagate
-        node.value += child_value
-        node.count += 1
+            child_path, leaf_board = self.select_leaf_node(child)
+            path += child_path
         self.board.pop()
-        return child_value
+        return path, leaf_board
 
     def select_max_ucb_child(self, node: Node):
         """get index of child node that maximizes ucb value"""
-        Q = np.repeat(0.5, len(node.children))
+        n = len(node.children)
+        Q = np.zeros(n)
         P = node.policy
-        N = np.zeros(len(node.children))
-        children_i = [i for i, c in enumerate(node.children) if c is not None]
+        C = np.zeros(n)
         for i, child in enumerate(node.children):
             if child is not None:
                 Q[i] = child.value / child.count
-                N[i] = child.count
-        U = Q + self.c_puct * P * (np.sqrt(node.count) / (1 + N))
+                C[i] = child.count
+        U = Q + self.c_puct * P * (np.sqrt(node.count) / (1 + C))
         return np.argmax(U)
 
-    def expand_and_evaluate_node(self, invert_value=False):
-        """create and evaluate node of current board position"""
+    def expand_node(self):
+        """create node of current board position and apply virtual loss"""
         z_hash = chess.polyglot.zobrist_hash(self.board)
         # check if node has already been reached
+        self.node_lookup_lock.acquire()
         if z_hash in self.node_lookup:
-            return self.node_lookup[z_hash]
+            node = self.node_lookup[z_hash]
         # otherwise create new node
-        node = Node(self.board)
-        self.node_lookup[z_hash] = node
-        # evaluate node
-        if self.board.is_checkmate():
-            node.value = -1
-        elif self.board.is_game_over():
-            node.value = 0
         else:
-            # TODO evaluate nodes in batches by layer on tree
-            state = get_board_state(self.board)
-            input_ids = torch.tensor([state], dtype=torch.long).cuda()
-            with torch.no_grad():
-                output = self.model(input_ids)
-                value = output['value'].detach().cpu().numpy()[0]
-                policy_logits = output['policy'].detach().cpu().numpy()[0]
-            legal_moves = [get_move(m, self.board.turn) for m in node.moves]
-            policy = softmax(policy_logits[legal_moves])
-            node.value = value
-            node.policy = policy
-        if invert_value:
-            node.value *= -1
+            node = Node(self.board)
+            self.node_lookup[z_hash] = node
+        self.node_lookup_lock.release()
+        # virtual loss
+        node.lock.acquire()
+        node.value -= 1
+        node.count += 1
+        node.lock.release()
         return node
+
+    def evaluate_node_batch(self, paths, boards):
+        """
+        evaluate batch of nodes with model and backpropagate
+
+        - nodes are taken from last element of each path
+        - paths are required for backpropagation
+        - boards are required for evaluation
+        """
+        states = [get_board_state(board) for board in boards]
+        policies, values = self.compute_policy_value_batch(states)
+        for i, x in enumerate(zip(paths, boards, policies, values)):
+            path, board, policy, value = x
+            node = path[-1]
+            # filter policy by legal moves
+            legal_moves = [get_move(m, board.turn) for m in node.moves]
+            policy = softmax(policy[legal_moves]) if legal_moves else []
+            # check node for terminal states
+            if board.is_checkmate():
+                value = -1
+            elif board.is_game_over():
+                value = 0
+            # apply policy and backpropagate value
+            node.lock.acquire()
+            node.policy = policy
+            node.lock.release()
+            for j, path_node in enumerate(path):
+                invert = 1 if j % 2 == 0 else -1
+                path_node.lock.acquire()
+                path_node.value += invert * value + 1
+                path_node.lock.release()
+
+    def compute_policy_value_batch(self, states):
+        """evaluate batch of board states on gpu"""
+        input_ids = torch.tensor(states, dtype=torch.long).cuda()
+        with torch.no_grad():
+            output = self.model(input_ids)
+            policy = output['policy'].detach().cpu().numpy()
+            value = output['value'].detach().cpu().numpy()
+        return policy, value
